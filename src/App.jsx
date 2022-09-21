add7883d1f2bb490f66f519b968535cdba820aa4
João Coimbra <joao.benatti.coimbra@gmail.com>
2022-09-21 11:25:45 -0300

    random bases title

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import './App.scss';
 
+import { useEffect, useState } from 'react';
+
 import Footer from './components/Footer';
 import Conversion from './components/Conversion';
 
 function App() {
+
+    const [count, setCount] = useState(0);
+    
+
+    useEffect(() => {
+        let bases = [2, 8, 10, 16]
+        let a = Math.floor(Math.random() * 4)
+        setTimeout(() => {
+            document.title = 'Convert'+bases[a]+'bases'
+            setCount(count+1)
+        }, 1000)
+    }, [count])
+
+    
+    // document.
+
     return (
         <div className="App">
             <h1>Convert<span>2</span>bases</h1>
